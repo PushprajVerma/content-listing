333feffc85f385766805f6b20120465fda441713
Pushpraj Verma <pushprajverma94@gmail.comcls>
2023-06-19 18:14:07 +0530

    deployment 5

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import movieReducer from './reducer'
+
+export default configureStore({
+  reducer: {
+    movies: movieReducer,
+  },
+  devTools: {
+    name: 'Content Listing',
+  },
+})
