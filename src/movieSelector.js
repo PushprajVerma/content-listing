0ce49a594a2534bc9b99bb56111739edae483f2e
Pushpraj Verma <pushprajverma94@gmail.comcls>
2023-06-19 15:41:18 +0530

    deploy new version

---
+++ src/movieSelector.js
@@ -0,0 +1,14 @@
+
+// import { useSelector } from 'react-redux'
+
+// const imageData = useSelector((state) => state.movies.imageData)
+// const filteredImages = useSelector((state) => state.movies.filteredImages)
+// const isLoading = useSelector((state) => state.movies.isLoading)
+// const currentPage = useSelector((state) => state.movies.currentPage)
+
+// export {
+//     imageData,
+//     filteredImages,
+//     isLoading,
+//     currentPage
+// }
