ea593f6605405ed360ad649be1f8435df5eda67e
Pushpraj Verma <pushprajverma94@gmail.comcls>
2023-06-19 17:28:22 +0530

    deployment 2

--- src/store.js
+++ src/store.js
@@ -1,18 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { createStore } from 'redux';
 import movieReducer from './reducer'
 
-// const store = createStore(
-//     movieReducer,
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   );
-
-// export default store;
 export default configureStore({
   reducer: {
     movies: movieReducer,
   },
   devTools: {
-    name: 'Your App Name',
+    name: 'Content Listing',
   },
 })
